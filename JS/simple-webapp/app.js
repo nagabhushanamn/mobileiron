362d67cd0e5daaad908a9e8a23c7e218204a378e
NAG <nag@NAGABHUSHANAMs-MacBook-Pro.local>
2017-01-31 17:08:35 +0530

    simple app

---
+++ JS/simple-webapp/app.js
@@ -0,0 +1,20 @@
+// using DOM API
+
+
+// document.addEventListener('DOMContentLoaded', function (e) {
+//     console.log('DOM ready...');
+
+//     var box = document.querySelector('.jumbotron');
+//     var showBtn = document.querySelector('.btn-primary');
+//     var hideBtn = document.querySelector('.btn-danger');
+
+//     showBtn.addEventListener('click', function (e) {
+//         box.style.display = '';
+//     });
+//     hideBtn.addEventListener('click', function (e) {
+//         box.style.display = 'none';
+//     });
+
+// });
+
+
